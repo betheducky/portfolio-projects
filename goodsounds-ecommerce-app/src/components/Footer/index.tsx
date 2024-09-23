3f8e3c8c84102b52a9ca6e6ebbd9fc75c9eb7e84
Aren Bruce <devtodaydevtomorrow@gmail.com>
2024-09-23 16:34:50 -0400

    Footer Construction

---
+++ goodsounds-ecommerce-app/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="wrap">
+            &copy;GoodSounds 2024
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
